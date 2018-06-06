4a01121492f3f9d982d1d3c80997dc1519a0a0ad
DWIZERO\DWiZero <cw316613@163.com>
2018-06-06 15:05:03 +0800

    修改：使得config微服务化，其他微服务可通过注册中心找到config

--- config-server/src/main/java/com/DWiZero/configServerApplication.java
+++ config-server/src/main/java/com/DWiZero/configServerApplication.java
@@ -3,12 +3,14 @@ package com.DWiZero;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * springCloud的服务注册中心
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class configServerApplication
 {
 
